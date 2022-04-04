0189c8138744f3c98c0b5ce22a6ab1322e3d3121
Amanda Gomes <88012982+AmandaGomesFe@users.noreply.github.com>
2022-04-04 16:08:18 -0300

    finalizei o curso JavaScript orientaçao a objetos

--- js-orientacao-objeto/index.js
+++ js-orientacao-objeto/index.js
@@ -1,42 +1,16 @@
-class Cliente{
-    nome;
-    cpf;
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-class ContaCorrente{
-    agencia;
-    saldo;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-    sacar(valor){
-      if(this.saldo >= valor){
-            this.saldo-= valor;
-        }  
-    }
-}
+const cliente2 = new Cliente("Alice", 88822233309);
 
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const conta2 = new ContaCorrente(102, cliente2);
 
+let valor = 200;
+contaCorrenteRicardo.transferir(200, conta2);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.saldo = 0;
-
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo)
-contaCorrenteRicardo.sacar(50);
-
-
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrente.numeroDeContas);
